439efa4dec70d83b2adbb1de42dd5b3498de2e9d
vedithathanneru <thanneruveditha@gmail.com>
2026-02-16 12:26:47 +0530

    Secure settings using environment variables

--- facerecog/settings.py
+++ facerecog/settings.py
@@ -10,8 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+
+
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +28,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-pw^%!*e$k*r_85!+b^8kgfjnb+v0x0f9%yrlybwa@ml=(=^l(e"
+# SECRET_KEY = "django-insecure-pw^%!*e$k*r_85!+b^8kgfjnb+v0x0f9%yrlybwa@ml=(=^l(e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 ALLOWED_HOSTS = ["10.14.20.241", "127.0.0.1" , "*"]
 
@@ -76,16 +86,30 @@ WSGI_APPLICATION = "facerecog.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "facenet",
+#         "USER": "root",
+#         "PASSWORD": "123$5",
+#         "HOST": "localhost",
+#         "PORT": "3306"
+#     }
+# }
+
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "facenet",
-        "USER": "root",
-        "PASSWORD": "123$5",
-        "HOST": "localhost",
-        "PORT": "3306"
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
+
 
 
 # Password validation
